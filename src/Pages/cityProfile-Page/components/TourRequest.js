4fc28d5303f6bd4aaaa0aca42935a945fe784f1d
Youssef Rashad <egyoussef_2000@hotmail.com>
2023-06-20 16:44:22 +0300

    added image to the form

--- src/Pages/cityProfile-Page/components/TourRequest.js
+++ src/Pages/cityProfile-Page/components/TourRequest.js
@@ -143,6 +143,7 @@ const TourRequest = () => {
   };
   return (
     <>
+      <div className={classes.formImgContainer}>
       <form onSubmit={handleOnSubmit}>
         <div className={classes.container}>
           <div className={classes.numContainer}>
@@ -326,6 +327,8 @@ const TourRequest = () => {
           </button>
         </div>
       </form>
+      <img className={classes.tourImg} src="https://img.freepik.com/free-vector/group-tourists-travelling-with-guide-holding-flag-flat-illustration_74855-18752.jpg?w=1380&t=st=1687267995~exp=1687268595~hmac=ec0d86394ea4c26659587cb41f7d75c2e84cbcdbf8e9acecd9661a3a6b8c6f6c"/>
+      </div>
       {showError && <p>Error Please Submit the required fields</p>}
     </>
   );
